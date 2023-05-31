aa6deef19bca524d20633bf57656b6811b9ef663
black9769 <black9769@dnx.kr>
2023-06-01 00:01:10 +0900

    더맵게(programers)

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,39 @@
 import java.util.*;
 
-//
+//array list로 풀이 실패
 public class Solution {
     public static void main(String[] args) {
-        solution(new int[]{93, 30, 55}, new int[]{1, 30, 5});
-        solution(new int[]{95, 90, 99, 99, 80, 99}, new int[]{1, 1, 1, 1, 1, 1});
+
+        solution(new int[]{1, 2, 3, 9, 10, 12}, 7);
+        solution(new int[]{1, 2, 3, 9, 10, 12}, 100);
+        solution(new int[]{1, 1, 2, 6}, 24);
+        solution(new int[]{4, 5, 2, 4}, 1);
     }
 
-    static List<Integer> solution(int[] progresses, int[] speeds) {
-        Stack<Integer> stack = new Stack<>(); //스택 생성
-        HashMap<Integer, Integer> hashMap = new HashMap<>(); //결과 값을 위한 Map
-        for (int i = 0; i < progresses.length; i++) { //스택에 결과 값을 넣는 과정
-            if (stack.empty() || stack.peek() < Math.ceil((100 - progresses[i]) / (double) speeds[i])) {
-                // 스택이 비거나 스택 상단 값이 그 다음에 들어갈 값보다 작으면 새로운 값을 넣어주고 맵을 생성해줌
-                stack.push((int) Math.ceil((100 - progresses[i]) / (double) speeds[i]));
-                hashMap.put((int) Math.ceil((100 - progresses[i]) / (double) speeds[i]), 1);
-            } else {
-                stack.push(stack.peek()); //그게 아닐시 상단 값을 넣어줌
-                if (hashMap.containsKey(stack.peek())) { //해쉬에 같은 값이 있으면 가능 배포 일을 하루 추가
-                    hashMap.put(stack.peek(), hashMap.get(stack.peek()) + 1);
-                }
-            }
+
+    static int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int i : scoville) {
+            priorityQueue.add(i);
+        }
+        int min = priorityQueue.peek();
+        while (K > min && priorityQueue.size() >1){
+            answer++;
+            int a = priorityQueue.poll();
+            int b = priorityQueue.poll();
+            int result = a + (b*2);
+            priorityQueue.add(result);
+            min = priorityQueue.peek();
         }
 
-        List<Integer> keySet = new ArrayList<>(hashMap.keySet()); // 맵을 키값으로 정렬하기 위해 리스트안에 넣어줌
-        Collections.sort(keySet); //키값으로 정렬
-        List<Integer> answer = new ArrayList<>(); //결과 값을 구하기 위한 리스트 생성
-        for (Integer integer : keySet) {
-            answer.add(hashMap.get(integer)); //정답에  value 추가
+        if (K>min){
+            return -1;
         }
+
+
         return answer;
     }
+
 
 }
