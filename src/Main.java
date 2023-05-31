866d4a31c4d01fb45bf02c561acadf41cd4b41d3
black9769 <black9769@dnx.kr>
2023-06-01 00:46:39 +0900

    2750(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -10,24 +10,8 @@ public class Main {
             A[i] = sc.nextInt();
         }
         Arrays.sort(A);
-        int M = sc.nextInt();
-        for (int i = 0; i < M; i++) {
-            System.out.println(binarySearch2(A, sc.nextInt(), 0, N - 1));
+        for (int i : A) {
+            System.out.println(i);
         }
-    }
-
-    static int binarySearch2(int[] A, int key, int low, int high) {
-        int mid;
-        while (low <= high) {
-            mid = (low + high) / 2;
-            if (key == A[mid]) {
-                return 1;
-            } else if (key < A[mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return 0; // 탐색 실패
     }
 }
