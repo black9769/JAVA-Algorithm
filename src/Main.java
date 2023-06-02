e0e1ac6477b9da1d3c6a1cb160a6114a17c613d5
black9769 <black9769@dnx.kr>
2023-06-02 16:55:52 +0900

    8단변속기(Softeer)

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,35 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main
 {
-    public static void main(String args[])  throws Exception
-    {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(in.readLine());
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int W = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-
-        ArrayList<int[]> list = new ArrayList<>();
-        for(int i=0;i<N;i++){
-            st = new StringTokenizer(in.readLine());
-            list.add(new int[]{Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())});
+        int[] dct = new int[8];
+        for(int i = 0 ; i < dct.length ; i++ ){
+            dct[i] = Integer.parseInt(st.nextToken());
         }
 
-
-        Collections.sort(list,(o1,o2)->o2[1]-o1[1]);
-
-        int answer = 0;
-
-        for(int[] item:list){
-            if(W<=item[0]){
-                answer += W*item[1];
-                break;
-            }else{
-                answer += item[0]*item[1];
-                W-=item[0];
+        if(dct[0] ==1){
+            boolean result = true;
+            for(int i = 0 ; i< dct.length ; i++){
+                if(dct[i] != i+1) result = false;
             }
+            System.out.printf("%s", result ?  "ascending": "mixed");
+
+        }
+        else if(dct[0] ==8){
+            boolean result = true;
+            for(int i = 0 ; i< dct.length ; i++){
+                if(dct[i]+i !=8) result = false;
+            }
+            System.out.printf("%s", result ?  "descending" :"mixed" );
         }
 
-        System.out.println(answer);
+        else  System.out.printf("%s", "mixed");
+
     }
 }
