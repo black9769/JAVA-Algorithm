92921955af38e3c9cc305d2fa8d3760a95d1b189
JUN <black9769@dnx.kr>
2023-06-14 18:30:01 +0900

    2741(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B =  sc.nextInt();
-        System.out.printf("%d", A + B);
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
     }
 }
