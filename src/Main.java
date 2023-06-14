485978fbde00be9a3c639202ce02b7977da6c722
JUN <black9769@dnx.kr>
2023-06-14 18:25:40 +0900

    2557(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String hex = st.nextToken();
-        System.out.println(Integer.parseInt(hex,16));
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
