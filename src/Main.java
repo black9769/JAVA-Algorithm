baf1087e060e8a5798e7932c6b4a1f6ba0d8b262
JUN <black9769@dnx.kr>
2023-06-14 18:39:10 +0900

    2752(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        for (int i = N; i > 0; i--) {
-            System.out.println(i);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        for(int i =0 ; i < arr.length ;i ++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
+        for (int i : arr) {
+            System.out.printf("%d " ,i);
+        }
+
     }
 }
