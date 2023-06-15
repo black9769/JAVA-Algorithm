3233b09b299c4ae47c9613e3a4a727ac222b246b
JUN <black9769@dnx.kr>
2023-06-15 10:00:24 +0900

    4101(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int year = sc.nextInt();
+    public static void main(String[] args)  {
+       Scanner sc = new Scanner(System.in);
 
-        if (year %4 ==0){
-            if (year %100 !=0 || year % 400 ==0) System.out.printf("1");
-            else System.out.printf("0");
+        while (true){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if (A ==0 && B== 0) break;
+            if(A>B) System.out.println("Yes");
+            else System.out.println("No");
+
         }
-        else System.out.printf("0");
-
     }
 }
