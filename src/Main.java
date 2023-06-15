613db01480dfc8c64f1142e46ef3985a4bc57a67
JUN <black9769@dnx.kr>
2023-06-16 00:24:46 +0900

    5217(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cnt =0;
-        while (true) {
-            cnt++;
-            int N0 = sc.nextInt();
-            if (N0 == 0) break;
-            int N1 = N0 * 3;
-            int N2;
-            if (N1 % 2 == 0) { //짝수
-                N2 = N1 / 2;
-                System.out.printf("%d. even",cnt);
-            } else {// 홀수
-                N2 = (N1 + 1) / 2;
-                System.out.printf("%d. odd",cnt);
+        int test_case = sc.nextInt();
+        for (int i = 0; i < test_case; i++) {
+            int n = sc.nextInt();
+            System.out.printf("Pairs for %d: ", n);
+            List<String> numList = new ArrayList<>();
+            for (int j = 1; j < 13; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (j + k == n) {
+                        String jStr = String.valueOf(j);
+                        String kStr = String.valueOf(k);
+                        String numStr = jStr + " " + kStr;
+                        numList.add(numStr);
+                    }
+                }
             }
-            int N3 = N2 * 3;
-            int N4 = N3/9;
-            System.out.printf(" %d", N4);
+            for (int j = 0; j < numList.size(); j++) {
+                System.out.printf("%s", numList.get(j));
+                if (j != numList.size()-1) System.out.printf(", ");
+            }
             System.out.println();
         }
+
     }
 }
