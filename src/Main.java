c1b1e8c4d4f78938396bcb51686bffa2b4deb4fa
JUN <black9769@dnx.kr>
2023-06-18 18:02:42 +0900

    8392(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n1 = sc.nextInt(), k1 = sc.nextInt(), n2 = sc.nextInt(), k2 = sc.nextInt();
-        System.out.println((n1 * k1) + (n2 * k2));
+        int n = sc.nextInt();
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        System.out.println(result);
     }
 }
