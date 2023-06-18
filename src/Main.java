5606bf9d1661b7dee2924d809dcbddd62c5506b5
JUN <black9769@dnx.kr>
2023-06-18 17:41:14 +0900

    6749(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int result = 0;
-        for (int i = 0; i < 5; i++) {
-            result += sc.nextInt();
-        }
+        int fst = sc.nextInt();
+        int sec = sc.nextInt();
+        int result = sec + (sec - fst);
         System.out.println(result);
     }
 }
