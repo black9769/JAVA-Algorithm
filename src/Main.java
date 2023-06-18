4dfbb3851df26c04ce2dadb0b07c11f34795c3ea
JUN <black9769@dnx.kr>
2023-06-18 17:58:38 +0900

    7891(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-
-        System.out.printf("The 1-3-sum is %d", 91 + a + b * 3 + c);
+        int tc = sc.nextInt();
+        for (int i = 0; i < tc; i++) {
+            long a = sc.nextInt();
+            long b = sc.nextInt();
+            System.out.println(a + b);
+        }
     }
 }
