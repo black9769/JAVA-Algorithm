2fc0af1861e0207784fe9916f1e1513c548b25e1
JUN <black9769@dnx.kr>
2023-06-19 11:09:31 +0900

    10093(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        for (int i = 0; i < tc; i++) {
-            int n = sc.nextInt();
-            int result = 0;
-            for (int j = 1; j <= n; j++) {
-                if (j%2 ==1) result += j;
-            }
-            System.out.println(result);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        if(b>a) {
+            System.out.println(b-a-1);
+            for(long i = a+1; i<b; i++) System.out.print(i+" ");
         }
+        else if(a>b) {
+            System.out.println(a-b-1);
+            for(long i = b+1; i<a; i++) System.out.print(i+" ");
+        }
+        else System.out.print(0);
     }
 }
