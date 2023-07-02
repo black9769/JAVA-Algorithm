1fb94e91643bacaaa038409fc6e202d211745db8
JUN <black9769@dnx.kr>
2023-07-02 21:34:30 +0900

    1010(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+
+    static int[][] dp = new int[30][30];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int TC = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < TC; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            System.out.println(combi(M, N));
+        }
     }
 
+    static int combi(int n, int r) {
+        // 이미 풀린 경우 바로 반환
+        if (dp[n][r] > 0) {
+            return dp[n][r];
+        }
+        // 2번 성질
+        if (n == r || r == 0) {
+            return dp[n][r] = 1;
+        }
+        // 1번 성질
+        return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
+    }
 }
