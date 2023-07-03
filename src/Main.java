c0a3d61b763dea8aee0122ad64f0cf6633028489
JUN <black9769@dnx.kr>
2023-07-03 17:14:23 +0900

    2941(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,21 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int cm = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < cm; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
-            switch (command) {
-                case "push":
-                    int x = Integer.parseInt(st.nextToken());
-                    stack.push(x);
-                    break;
-                case "pop":
-                    System.out.println(stack.size() == 0 ? -1 : stack.pop());
-                    break;
-                case "size":
-                    System.out.println(stack.size());
-                    break;
-                case "empty":
-                    System.out.println(stack.size() == 0 ? 1 : 0);
-                    break;
-                case "top":
-                    System.out.println(stack.size() == 0 ? -1 : stack.peek());
-                    break;
-            }
-        }
+        String alphabet = br.readLine();
+        String s = alphabet.replace("c-", "c");
+        s = s.replace("c=", "c");
+        s = s.replace("dz=", "d");
+        s = s.replace("d-", "d");
+        s = s.replace("lj", "l");
+        s = s.replace("nj", "n");
+        s = s.replace("s=", "s");
+        s = s.replace("z=", "z");
+
+        int len = s.length();
+
+        System.out.println(len);
     }
+
 }
