e45a58ea777622ed96e7ad4e0ea1e09bad2d6336
JUN <black9769@dnx.kr>
2023-07-03 16:54:27 +0900

    10828(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        for (int i = 0; i < tc; i++) {
-            int ans = 0;
+        int cm = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < cm; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int startX = Integer.parseInt(st.nextToken());
-            int startY = Integer.parseInt(st.nextToken());
-            int endX = Integer.parseInt(st.nextToken());
-            int endY = Integer.parseInt(st.nextToken());
-            int planetCount = Integer.parseInt(br.readLine());
-            for (int j = 0; j < planetCount; j++) {
-                st = new StringTokenizer(br.readLine());
-                int planetX = Integer.parseInt(st.nextToken());
-                int planetY = Integer.parseInt(st.nextToken());
-                int r = Integer.parseInt(st.nextToken());
-                boolean startCheck = false;
-                boolean endCheck = false;
-                if (Math.pow(r, 2) >= (Math.pow(startX - planetX, 2) + Math.pow(startY - planetY, 2))) {
-                    startCheck = true;
-                }
-                if (Math.pow(r, 2) >= (Math.pow(endX - planetX, 2) + Math.pow(endY - planetY, 2))) {
-                    endCheck = true;
-                }
-                if (startCheck && !endCheck) ans++;
-                else if (!startCheck && endCheck) ans++;
+            String command = st.nextToken();
+            switch (command) {
+                case "push":
+                    int x = Integer.parseInt(st.nextToken());
+                    stack.push(x);
+                    break;
+                case "pop":
+                    System.out.println(stack.size() == 0 ? -1 : stack.pop());
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    System.out.println(stack.size() == 0 ? 1 : 0);
+                    break;
+                case "top":
+                    System.out.println(stack.size() == 0 ? -1 : stack.peek());
+                    break;
             }
-            System.out.println(ans);
         }
-
-
     }
 }
