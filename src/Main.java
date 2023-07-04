d77dee75b7114d4a835109b3d4fa9ac839ea577c
JUN <black9769@dnx.kr>
2023-07-04 12:52:39 +0900

    1931(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
+import java.util.Comparator;
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int k = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        int[] money = new int[k];
-        for (int i = k - 1; i >= 0; i--) {
-            st = new StringTokenizer(br.readLine());
-            money[i] = Integer.parseInt(st.nextToken());
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+		/*
+		  time[][0] 은 시작시점을 의미
+		  time[][1] 은 종료시점을 의미
+		*/
+        int[][] time = new int[N][2];
+
+
+        for(int i = 0; i < N; i++) {
+            time[i][0] = in.nextInt();	// 시작시간
+            time[i][1] = in.nextInt();	// 종료시간
         }
-        int cnt = 0;
-        for (int i = 0; i < k; i++) {
-            if (n >= money[i]) {
-                cnt = cnt + (n / money[i]);
-                n %= money[i];
+
+
+        // 끝나는 시간을 기준으로 정렬하기 위해 compare 재정의
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                // 종료시간이 같을 경우 시작시간이 빠른순으로 정렬해야한다.
+                if(o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        int count = 0;
+        int prev_end_time = 0;
+
+        for(int i = 0; i < N; i++) {
+            // 직전 종료시간이 다음 회의 시작 시간보다 작거나 같다면 갱신
+            if(prev_end_time <= time[i][0]) {
+                prev_end_time = time[i][1];
+                count++;
             }
         }
-        System.out.println(cnt);
+
+        System.out.println(count);
     }
+
 }
