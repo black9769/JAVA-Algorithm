5334accd8106f3bee4844fe475922fa75ba35c6f
JUN <black9769@dnx.kr>
2023-07-05 20:10:25 +0900

    1946(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long S = sc.nextLong();
-        long N = 0;
-        long i = 0;
-        while (true) {
-            i++;
-            N = N + i;
-            if (N > S) {
-                break;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        for (int i = 0; i < tc; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int count = Integer.parseInt(st.nextToken());
+            int[][] person = new int[count][2];
+            for (int j = 0; j < count; j++) {
+                st = new StringTokenizer(br.readLine());
+                person[j][0] = Integer.parseInt(st.nextToken());
+                person[j][1] = Integer.parseInt(st.nextToken());
             }
+            Arrays.sort(person, new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return Integer.compare(o1[0], o2[0]);
+                }
+            });
+            checkHap(person);
         }
-        System.out.println(i - 1);
+    }
 
+    private static void checkHap(int[][] person) {
+        int pivot = person[0][1];
+        int cnt = 1; // 정렬된 첫번째 사람은 채용
+        for (int i = 0; i < person.length; i++) {
+            if (person[i][1] < pivot) {
+                pivot = person[i][1];
+                cnt++;
+            }
 
+        }
+        System.out.println(cnt);
     }
 }
