f196552d78050481aab7783ecc21d0515fd49dd5
black9769 <black9769@dnx.kr>
2023-07-06 12:06:36 +0900

    1110(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,43 @@
-import java.io.*;
 import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        String strN = String.valueOf(N);
-        char[] arrN = strN.toCharArray();
-        int compare = 0;
+        String N = sc.next();
+        int number = Integer.parseInt(N);
+        char[] arrN = N.toCharArray();
+        int[] num = new int[2];
+        if (number < 10) {
+            num[1] = number;
+        } else {
+            for (int i = 0; i < num.length; i++) {
+                num[i] = arrN[i] - '0';
+            }
+        }
         int cnt = 0;
         while (true) {
-            for (char n : arrN) {
-                compare = compare + (int) n;
-                arrN = swap(arrN, compare);
+            int sum;
+            if (num[0] + num[1] < 10) {
+                sum = num[0] + num[1];
+            } else {
+                sum = (num[0] + num[1]) % 10;
             }
-            int compareN = Integer.parseInt(String.valueOf(arrN));
-            if (N == compareN) break;
+            num = swap(num, sum);
+            int aa = makeInteger(num);
             cnt++;
+            if (number == aa) break;
         }
         System.out.println(cnt);
     }
 
-    static char[] swap(char[] arrN, int i) {
-        arrN[0] = arrN[1];
-        arrN[1] = (char) i;
-        return arrN;
+    static int[] swap(int[] arr, int sum) {
+        arr[0] = arr[1];
+        arr[1] = sum;
+        return arr;
     }
+
+    static int makeInteger(int[] num) {
+        return num[0] * 10 + num[1];
+    }
+
 }
