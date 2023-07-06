1cf402725f0582feb5c8a0831dd81e4fad2ce2ee
black9769 <black9769@dnx.kr>
2023-07-06 15:02:17 +0900

    이진변환반복하기(programers)

--- src/Solution.java
+++ src/Solution.java
@@ -3,37 +3,30 @@ import java.util.*;
 //array list로 풀이 실패
 public class Solution {
     public static void main(String[] args) {
-        solution(3,new int[][]{{1,1,0},{1,1,0},{0,0,1}});
-        solution(3,new int[][]{{1,1,0},{1,1,0},{0,0,1}});
+        solution("110010101001");
+        solution("01110");
+        solution("1111111");
     }
 
 
-    public static int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] visited = new boolean[computers.length];
-        for (int i =0 ; i <computers.length ; i++){
-            visited[i] = false;
-        }
-
-        for (int i =0 ; i< computers.length;i++){
-            if (visited[i]== false){
-                answer++;
-                dfs(i,visited,computers);
+    public static int[] solution(String s) {
+        int[] answer = new int[2];
+        int cnt = 0;
+        int count = 0;
+        while (true) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) - '0' == 0) {
+                    cnt++;
+                }
             }
+            count++;
+            s = s.replaceAll("[^1]", "");
+            s = Integer.toBinaryString(s.length());
+            if (s.equals("1")) break;
         }
+        answer[0] = count;
+        answer[1] = cnt;
 
         return answer;
     }
-
-    static void dfs(int node, boolean[] visited, int[][] computer){
-        visited[node] =true;
-        for (int i = 0; i < computer.length; i++) {
-            if (visited[i] ==false && computer[node][i] ==1){
-                dfs(i,visited,computer);
-            }
-
-        }
-    }
-
-
 }
