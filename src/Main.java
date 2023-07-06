18831ee96eed33a3b01e110f5fd4083c74df8a3b
JUN <black9769@dnx.kr>
2023-07-06 11:22:12 +0900

    1110(BOJ) -ing

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        for (int i = 0; i < tc; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int count = Integer.parseInt(st.nextToken());
-            int[][] person = new int[count][2];
-            for (int j = 0; j < count; j++) {
-                st = new StringTokenizer(br.readLine());
-                person[j][0] = Integer.parseInt(st.nextToken());
-                person[j][1] = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String strN = String.valueOf(N);
+        char[] arrN = strN.toCharArray();
+        int compare = 0;
+        int cnt = 0;
+        while (true) {
+            for (char n : arrN) {
+                compare = compare + (int) n;
+                arrN = swap(arrN, compare);
             }
-            Arrays.sort(person, new Comparator<int[]>() {
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    return Integer.compare(o1[0], o2[0]);
-                }
-            });
-            checkHap(person);
-        }
-    }
-
-    private static void checkHap(int[][] person) {
-        int pivot = person[0][1];
-        int cnt = 1; // 정렬된 첫번째 사람은 채용
-        for (int i = 0; i < person.length; i++) {
-            if (person[i][1] < pivot) {
-                pivot = person[i][1];
-                cnt++;
-            }
-
+            int compareN = Integer.parseInt(String.valueOf(arrN));
+            if (N == compareN) break;
+            cnt++;
         }
         System.out.println(cnt);
+    }
+
+    static char[] swap(char[] arrN, int i) {
+        arrN[0] = arrN[1];
+        arrN[1] = (char) i;
+        return arrN;
     }
 }
