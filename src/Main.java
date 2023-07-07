351127059e80574fef690a678fe6d0ae02f4ccf4
black9769 <black9769@dnx.kr>
2023-07-07 12:20:27 +0900

    1427(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        String[] strArr = new String[N];
-        for (int i = 0; i < N; i++) {
-            String str = sc.next();
-            strArr[i] = str;
+        String str = String.valueOf(N);
+        int len = str.length();
+        Integer[] arr = new Integer[len];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = N % 10;
+            N /= 10;
         }
-        String[] resultArr = Arrays.stream(strArr).distinct().toArray(String[]::new);
-        Arrays.sort(resultArr, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                if (s1.length() == s2.length()) return s1.compareTo(s2);
-                else return s1.length() - s2.length();
-            }
-        });
-
-        for (String str : resultArr){
-            System.out.println(str);
+        Arrays.sort(arr, Collections.reverseOrder());
+        for (Integer i : arr){
+            System.out.printf("%d", i);
         }
     }
 }
