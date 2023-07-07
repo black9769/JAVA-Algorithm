554ecd04310384f620065af4a986be85cc47be35
black9769 <black9769@dnx.kr>
2023-07-07 12:37:47 +0900

    11650(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,30 @@
-import java.io.BufferedReader;
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        String str = String.valueOf(N);
-        int len = str.length();
-        Integer[] arr = new Integer[len];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = N % 10;
-            N /= 10;
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        int[][] arr = new int[N][2];
+
+        for(int i = 0; i < N; i++) {
+            arr[i][0] = in.nextInt();
+            arr[i][1] = in.nextInt();
         }
-        Arrays.sort(arr, Collections.reverseOrder());
-        for (Integer i : arr){
-            System.out.printf("%d", i);
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if(e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        for(int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
         }
     }
 }
