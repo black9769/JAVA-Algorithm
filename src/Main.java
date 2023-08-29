32131de925c796d8a53bc018bbe3586d75310075
black9769 <black9769@naver.com>
2023-08-29 21:11:33 +0900

    2577(BOJ)

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int f = sc.nextInt();
-        n = n - (n % 100);
-        while (true) {
-            if (n % f == 0) {
-                break;
-            } else {
-                n++;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        String mul = String.valueOf(a * b * c);
+        int[] arr = new int[10];
+        for (int i = 0; i < mul.length(); i++) {
+            int num = mul.charAt(i) - 48 ;
+            for (int j = 0; j < arr.length ; j++){
+                if (j == num)  {
+                    arr[j] ++;
+                    break;
+                }
             }
         }
-        int ans = n % 100;
-        System.out.printf("%02d", ans);
-        System.out.println();
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
 }
