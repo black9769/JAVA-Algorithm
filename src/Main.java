f67bcea7e613eb9469e04c18cd3eea1dcf1c0fc2
black9769 <black9769@naver.com>
2023-08-29 20:39:45 +0900

    271037OJ) b

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        for (int t = 0; t < tc; t++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            st = new StringTokenizer(br.readLine());
-            int b = Integer.parseInt(st.nextToken());
-            int[] ho = new int[b];
-            int[] dp = new int[b];
-            for (int i = 1; i <= b; i++) {
-                ho[i - 1] = i;
-            }
-            while (a-- > 0) {
-                dp[0] = 1;
-                for (int i = 1; i < dp.length; i++) {
-                    dp[i] = dp[i - 1] + ho[i];
-                    ho[i] =dp[i];
-                }
-            }
-            System.out.println(dp[b - 1]);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-
-
+        Arrays.sort(arr);
+        if (n == 1) {
+            System.out.println(arr[0] * arr[0]);
+        } else {
+            System.out.println(arr[0] * arr[arr.length - 1]);
+        }
     }
 }
