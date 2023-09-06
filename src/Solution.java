5ef695c38468015016066e71fa644a398a6bfafd
JUN <black9769@dnx.kr>
2023-09-06 13:17:56 +0900

    programmers 최고의 집합

--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,24 @@
 import java.util.*;
+
 //스케듈링을 하는 알고리즘이랑 동일
 public class Solution {
     public static void main(String[] args) {
-        solution(new int[][]{{4, 5}, {4, 8}, {10, 14}, {11, 13}, {5, 12}, {3, 7}, {1, 4}});
+        solution(2, 9);
+        solution(2, 2);
+        solution(2, 1);
+        solution(2, 8);
     }
 
-
-    public static int solution(int[][] targets) {
-        int answer = 0;
-        //종점을 기준으로 오름차순 정렬
-        Arrays.sort(targets, (o1, o2) -> {
-            if(o1[1] == o2[1]){
-                return o1[0] - o2[0];
-            }
-            return o1[1] - o2[1];
-        });
-        //첫번째를 기준으로 미사일 요격
-        int end = targets[0][1];
-        answer++; // 첫 번째로 만들어지는 요격 시스템
-
-        for(int[] tar : targets){
-            if(tar[0] >= end){
-                end = tar[1];
-                answer++; // 시점이 요격 시스템의 상한보다 오른쪽에 있기 때문에 새로운 요격 시스템 추가
-            }
+    public static int[] solution(int n, int s) {
+        if(n > s) return new int[]{-1};
+        int[] answer= new int[n];
+        Arrays.fill(answer, s / n);
+        for (int i = 0; i < s%n; i++) {
+            answer[i] ++;
         }
+
+        Arrays.sort(answer);
+
         return answer;
     }
 
