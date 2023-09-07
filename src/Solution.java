661f418449c676c46f2643837f8a834fcf0748a9
black9769 <black9769@naver.com>
2023-09-07 15:38:42 +0900

    programmers 카펫

--- src/Solution.java
+++ src/Solution.java
@@ -3,25 +3,28 @@ import java.util.*;
 //짝이 들어간다는 문제는 대부분 스택구조로 풀수 있음
 public class Solution {
     public static void main(String[] args) {
-        solution(new int[][]{{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}});
+        solution(10, 2);
+        solution(8, 1);
+        solution(24, 24);
+        solution(12, 3);
+        solution(9, 6);
     }
 
 
-    public static int solution(int[][] triangle) {
-        int answer =0;
-        for(int i = 1; i < triangle.length ; i++) {
-            for(int j = 0; j < triangle[i].length ; j++) {
-                if(j == 0) {//왼쪽 끝
-                    triangle[i][j] += triangle[i-1][j];
+    public static int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int size = brown + yellow; //타일의 갯수가 곧 사이즈
+        for (int i = size; i > 0; i--) { //가장 큰수부터 반복문 시작 -> 가로가 크기때문
+            if (size % i == 0 && i >= size / i && size / i > 2) { //자연수로 이루어져있기때문에 나머지가 0이고 가로가 더 크고 세로가 갈색 테두리를 포함해야하기 때문에 2보다 항상 커야함
+                int l = i; //가로가 큰 순서대로 i가 가로길이가 됨
+                int h = size / i; //사각형 크기 공식에 따라 크기에서 i를 나누면 높이가됨
+                if (brown == (l + h) * 2 - 4) { //사각형의 둘레를 구하는 공식에서 모서리가 4번이상 덧셈에 들어갔기때문에 4를 제외시켜줌
+                    answer[0] =l;
+                    answer[1] =h;
+                    break;
                 }
-                else if(j == i) {//오른쪽 끝
-                    triangle[i][j] += triangle[i-1][j-1];
-                }
-                else {
-                    triangle[i][j] += Math.max(triangle[i-1][j], triangle[i-1][j-1]);
-                }
-                answer = Math.max(answer, triangle[i][j]);
             }
+
         }
         return answer;
     }
