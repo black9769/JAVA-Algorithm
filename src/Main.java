f36683fd22b3ba89a95e37002cad1ae3afe8fe05
black9769 <black9769@naver.com>
2023-09-08 00:12:42 +0900

    programmersㅠãBOJ 마인크래ã프트

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,56 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();//시험장수
-        long[] arr = new long[n];//시험장
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int[][] land = new int[n][m];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();//시험장 총원
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < m; j++) {
+                land[i][j] = Integer.parseInt(st.nextToken());
+                min = Math.min(min, land[i][j]);
+                max = Math.max(max, land[i][j]);
+            }
         }
-        int b = sc.nextInt(); //총감독이 감독할 수있는 인원수
-        int c = sc.nextInt(); //서브감독이 감독할 수 있는 인원수
-        long total = 0;
-        for (int i = 0; i < n; i++) {
-            arr[i] = arr[i] - b;//총감독이 감독하고 남은 인원수
+
+        int minTime = Integer.MAX_VALUE;
+        int height = 0;
+
+        for (int i = min; i <= max; i++) {
+            int blocks = b;
+            int time = 0;
+
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < m; k++) {
+                    if (land[j][k] > i) { //블록 제거 2초
+                        time += (land[j][k] - i) * 2;
+                        blocks += (land[j][k] - i);
+                    } else if (land[j][k] < i){ //블록 쌓기 1초
+                        time += (i - land[j][k]);
+                        blocks -= (i - land[j][k]);
+                    }
+                }
+            }
+
+            if (blocks >= 0 && minTime >= time) {
+                minTime = time;
+                height = i;
+            }
         }
-        total += n;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] <= 0) continue;
-            long num =  (long)Math.ceil((double) arr[i] / (double)c);
-            total += num;
-        }
-        System.out.println(total);
+
+        System.out.println(minTime + " " + height);
     }
 }
