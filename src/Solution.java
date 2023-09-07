cffbed92348308d0f8b460f89b8bba74c11f8384
black9769 <black9769@naver.com>
2023-09-07 16:47:36 +0900

    programmers ì¹여ãì연ì속 펄스 부분 수열의 합 ãlv3

--- src/Solution.java
+++ src/Solution.java
@@ -1,31 +1,45 @@
 import java.util.*;
 
-//짝이 들어간다는 문제는 대부분 스택구조로 풀수 있음
+//동적 프로그래밍의 기본적인 문제 lv3
 public class Solution {
     public static void main(String[] args) {
-        solution(10, 2);
-        solution(8, 1);
-        solution(24, 24);
-        solution(12, 3);
-        solution(9, 6);
+        solution(new int[]{2, 3, -6, 1, 3, -1, 2, 4});
     }
 
 
-    public static int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int size = brown + yellow; //타일의 갯수가 곧 사이즈
-        for (int i = size; i > 0; i--) { //가장 큰수부터 반복문 시작 -> 가로가 크기때문
-            if (size % i == 0 && i >= size / i && size / i > 2) { //자연수로 이루어져있기때문에 나머지가 0이고 가로가 더 크고 세로가 갈색 테두리를 포함해야하기 때문에 2보다 항상 커야함
-                int l = i; //가로가 큰 순서대로 i가 가로길이가 됨
-                int h = size / i; //사각형 크기 공식에 따라 크기에서 i를 나누면 높이가됨
-                if (brown == (l + h) * 2 - 4) { //사각형의 둘레를 구하는 공식에서 모서리가 4번이상 덧셈에 들어갔기때문에 4를 제외시켜줌
-                    answer[0] =l;
-                    answer[1] =h;
-                    break;
-                }
-            }
+    public static long solution(int[] sequence) {
+        long answer = 0;
+        int size = sequence.length;
 
+        //펄스 연산을 하게 되면 두가지 조건으로만 나올수 있다.
+        int[] a = new int[size];
+        for (int i = 0; i < size; i++) {
+            if (i % 2 == 0) a[i] = sequence[i];
+            else a[i] = sequence[i] * -1;
         }
+        int[] b = new int[size];
+        for (int i = 0; i < size; i++) {
+            if (i % 2 == 0) b[i] = sequence[i] * -1;
+            else b[i] = sequence[i];
+        }
+        //최대값을 dp에 넣어서 비교한다.
+        long[] dp1 = new long[size];
+        long[] dp2 = new long[size];
+
+        dp1[0] = a[0];
+        dp2[0] = b[0];
+        answer = Math.max(dp1[0], dp2[0]);
+
+        for (int i = 1; i < size; i++) {
+            dp1[i] = Math.max(dp1[i-1] + a[i] , a[i]);
+            dp2[i] = Math.max(dp2[i-1] + b[i] , b[i]);
+
+
+            long max = Math.max(dp1[i], dp2[i]);
+            answer = Math.max(answer, max);
+        }
+
+
         return answer;
     }
 
