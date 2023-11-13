f4b2d36bc6b309de42a690d8f032e3e6ad04da9c
black9769 <black9769@naver.com>
2023-11-13 21:36:12 +0900

    알고리즘 - 선택정렬

--- src/SortAlgorithm.java
+++ src/SortAlgorithm.java
@@ -2,38 +2,29 @@ import java.util.Arrays;
 
 public class SortAlgorithm {
     static int[] arr = new int[]{7, 1, 3, 2, 4, 5, 6};
+    static int cnt =0;
 
     public static void main(String[] args) {
-        sortBubble(arr);
+        selectionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void sortBubble(int[] arr) {
-        boolean isSwap; //교환 여부
-        do {
-            isSwap = false;
-            for (int i = 0; i < arr.length - 2; i++) { //시작부터 순회
-                if (arr[i] > arr[i + 1]) {
-                    swap(i, i + 1);
-                    isSwap = true; //교환 발생
+    private static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length -1; i++) {
+            int min_idx = i; //
+            for (int j = i +1; j < arr.length; j++) {
+                if (arr[j] < arr[min_idx]){
+                    min_idx =j;
                 }
             }
-            if (!isSwap) {
-                break;
-            }
-            isSwap = false;
-            for (int i = arr.length - 2; i >= 0; i--) { //끝부터 순회
-                if (arr[i] > arr[i + 1]) {
-                    swap(i, i + 1);
-                    isSwap = true;
-                }
-            }
-        } while (isSwap);
+            swap(min_idx , i); //최솟값과 맨 앞 위치한 값을 교체한다
+        }
     }
 
     private static void swap(int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+        cnt++;
     }
 }
